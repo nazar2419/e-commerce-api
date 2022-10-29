e80c5853d08d0bcab5d69e03598353cdce027208
nazar2419 <nazarx095@gmail.com>
2022-10-29 16:23:57 +0300

    auth complete

---
+++ models/Product.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, 'Please provide product name'],
+    maxlength: [100, 'Name can not be more than 100 characters'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Please provide product price'],
+    default: 0,
+  },
+  description: {
+    type: String,
+    required: [true, 'Please provide product description'],
+    maxlength: [100, 'Name can not be more than 100 characters'],
+  },
+  image: {
+    type: String,
+    default: '/uploads/example.jpeg',
+  }
+});
