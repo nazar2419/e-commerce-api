895c6012ffd6de021be2d131767b462789a3df90
nazar2419 <nazarx095@gmail.com>
2022-11-01 18:37:04 +0200

    product routes - structure

--- models/Product.js
+++ models/Product.js
@@ -20,5 +20,48 @@ const ProductSchema = new mongoose.Schema({
   image: {
     type: String,
     default: '/uploads/example.jpeg',
-  }
-});
+  },
+  category: {
+    type: String,
+    required: [true, 'Please provide product category '],
+    enum: ['office', 'kitchen', 'bedroom'],
+  },
+  company: {
+    type: String,
+    required: [true, 'Please provide company '],
+    enum: {
+      values: ['ikea', 'liddy', 'marcos'],
+      message: '{VALUE} is not supported'
+    },
+  },
+  colors: {
+    type: [String],
+    required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  freeShipping: {
+    type: Boolean,
+    default: false,
+  },
+  inventory: {
+    type: Number,
+    required: true,
+    default: 15,
+  },
+  averageRating: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+},
+{ timestamps: true }
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
